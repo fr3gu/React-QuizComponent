7abfb4ce447401db6f91cb2928f3f49d9e141c16
Fredrik Olsson <fr3gu77@gmail.com>
2018-09-13 08:52:28 +0200

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,10 +5,13 @@ import QuizEnd from "./QuizEnd";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+    
     constructor(props) {
         super(props);
 
-        this.state = { quiz_position: 1 };
+        this.state = {
+            quiz_position: 1
+        };
     }
 
     showNextQuestion() {
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
-    handleClick(buttonText) {
-        if(buttonText === this.props.quiz_question.answer)
-            this.props.showNextQuestionHandler();
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            incorrectAnswer: false
+        };
     }
+
+    handleClick(buttonText) {
+        if(buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer : false});
+        } else {
+            this.setState({incorrectAnswer : true});
+        }
+    }
+
     render() {
         return (
             <main>
@@ -18,6 +32,7 @@ class QuizQuestion extends Component {
                             return (<QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)} />);
                         })}
                     </ul>
+                    <p className="error">{this.state.incorrectAnswer ? "Sorry, that's not right" : null}</p>
                 </section>
             </main>
         );
